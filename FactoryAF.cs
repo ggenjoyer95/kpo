57a6f2dd090d531ac3fd52f7db24f5cea63633c1
ggenjoyer <ggenjoyer@gmail.com>
2025-01-19 20:35:54 +0300

    Initial commit

---
+++ FactoryAF.cs
@@ -0,0 +1,33 @@
+﻿namespace S1.PedalCarAccauntingInformationSystem;
+
+public class FactoryAF
+{
+    public List<Car> Cars { get; private set; }
+
+    public Queue<Customer> Customers { get; private set; }
+
+    public FactoryAF(IEnumerable<Customer> customers)
+    {
+        Customers = new Queue<Customer>(customers);
+        Cars = new List<Car>();
+    }
+
+    internal void SaleCar()
+    {
+        while (Customers.Count > 0 && Cars.Count > 0)
+        {
+            var customer = Customers.Dequeue();
+            var car = Cars.Last();
+            customer.Car = car;
+            Cars.RemoveAt(Cars.Count - 1);
+        }
+
+        Cars.Clear();
+    }
+
+    internal void AddCar()
+    {
+        var car = new Car { Number = Cars.Count + 1 };
+        Cars.Add(car);
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿namespace S1.PedalCarAccauntingInformationSystem;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        var customers = new List<Customer>
+        {
+            new() { Name = "Ivan" },
+            new() { Name = "Petr" },
+            new() { Name = "Sidr" },
+        };
+
+        var factory = new FactoryAF(customers);
+
+        for (int i = 0; i < 5; i++)
+            factory.AddCar();
+
+        Console.WriteLine("Машины на складе:");
+        Console.WriteLine(string.Join(Environment.NewLine, factory.Cars));
+        Console.WriteLine("Клиенты в очереди:");
+        Console.WriteLine(string.Join(Environment.NewLine, customers));
+
+        factory.SaleCar();
+
+        Console.WriteLine("\nПосле отработки SaleCar:");
+        Console.WriteLine("Машины на складе:");
+        Console.WriteLine(string.Join(Environment.NewLine, factory.Cars));
+        Console.WriteLine("Клиенты, получившие машины:");
+        Console.WriteLine(string.Join(Environment.NewLine, customers));
+    }
+}
