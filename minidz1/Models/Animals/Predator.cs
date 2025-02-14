2719036ba1e2a99acc2d19d39711288488c73371
ggenjoyer <ggenjoyer@gmail.com>
2025-02-14 23:08:33 +0300

    minidz1

--- minidz1/Models/Animals/Predator.cs
+++
@@ -1,7 +0,0 @@
-namespace Zoopark;
-
-public abstract class Predator : Animal
-{
-    protected Predator(int food, int health, int number, string? name = null)
-        : base(food, health, number, name) { }
-}
--- minidz1/Models/Animals/Wolf.cs
+++
@@ -1,7 +0,0 @@
-namespace Zoopark;
-
-public class Wolf : Predator
-{
-    public Wolf(int food, int health, int number, string? name = null)
-        : base(food, health, number, name) { }
-}
